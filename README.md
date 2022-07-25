f3beaa2668ad22739ea62c3ca6f54da717a0a4f0
TheBanusco10 <thebanusco10@gmail.com>
2022-07-25 18:45:31 +0200

    WIP. Title and description added to Dashboard. JS Validation for inputs

---
+++ README.md
@@ -0,0 +1,7 @@
+# User Notifications
+
+A Wordpress plugin to send notifications to users.
+
+## Changelog
+
+- 1.0.0 Basic behaviour
--- src/Views/dashboard.php
+++ src/Views/dashboard.php
@@ -1,50 +1,47 @@
 <?php
-
-use UserNotifications\Classes\NotificationCPT;
-
 $users = get_users( [
 	'exclude' => [ get_current_user_id() ]
 ] );
-
-//$notification = get_option('notification');
-$notifications = new WP_Query( [
-	'post_type'  => NotificationCPT::POST_TYPE,
-	'meta_query' => [
-        [
-	        'key'     => 'users',
-	        'value'   => '3',
-	        'compare' => 'LIKE'
-        ]
-	]
-] );
-
-var_dump($notifications->get_posts());
-var_dump(get_post_meta($notifications->get_posts()[0]->ID, 'users', true));
-
 ?>
 
 <h1>User Notifications - Dashboard</h1>
 
 <div class="un__dashboard">
-    <p class="un__users-selected-container">
-        Selected: <span class="un__users-selected">0</span> / <?= sizeof($users) ?>
-        <button data-button="un__select-all-users">Select all</button>
-    </p>
 
-    <section class="un__users">
-        <?php foreach ($users as $user): ?>
-            <div class="un__user" data-id="<?= $user->ID ?>">
-                <p class="un__name">
-                    <?= $user->display_name ?>
-                </p>
-                <p class="un__email">
-                    <?= $user->user_email ?>
-                </p>
-            </div>
-        <?php endforeach; ?>
+    <section id="un__notification-content">
+        <div>
+            <label for="un__notification-title">Title</label>
+            <input type="text" id="un__notification-title">
+        </div>
+
+        <div>
+            <label for="un__notification-content">Content</label>
+            <textarea id="un__notification-description"></textarea>
+        </div>
     </section>
 
-    <button data-button="un__sendNotification">Send notification</button>
+    <section id="un__users-wrapper">
+        <p class="un__users-selected-container">
+            Selected: <span class="un__users-selected">0</span> / <?= sizeof($users) ?>
+            <button class="button" data-button="un__select-all-users">Select all</button>
+        </p>
+
+        <section class="un__users">
+		    <?php foreach ($users as $user): ?>
+                <div class="un__user" data-id="<?= $user->ID ?>">
+                    <p class="un__name">
+					    <?= $user->display_name ?>
+                    </p>
+                    <p class="un__email">
+					    <?= $user->user_email ?>
+                    </p>
+                </div>
+		    <?php endforeach; ?>
+        </section>
+    </section>
+
+
+    <button class="button button-disabled" data-button="un__sendNotification">Send notification</button>
 </div>
 
 
